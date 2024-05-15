7efe1a2f09dc0ff40d57f6aedc12e49f0da220b9
rafaelqadv <Rafaelq@rvadv.com>
2024-05-15 13:30:28 -0400

    post template init

--- rvadv_post-templates/rvadv_post-templates/rvadv_post-templates.php
+++ rvadv_post-templates/rvadv_post-templates/rvadv_post-templates.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: RVADV Template Buttons for Blog Posts
 Description: Adds custom template buttons to the post editor to insert predefined HTML snippets.
-Version: 1.0.3
+Version: 1.0
 Author: Rafael Q
 Author URI: http://rvadv.com
 */
